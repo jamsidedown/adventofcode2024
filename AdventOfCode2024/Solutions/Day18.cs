95dec57a8ae30715d6389bb71ed2adc8fe379719
jamsidedown <46314954+jamsidedown@users.noreply.github.com>
2024-12-18 13:22:03 +0000

    Refactored D18

--- AdventOfCode2024/Solutions/Day18.cs
+++ AdventOfCode2024/Solutions/Day18.cs
@@ -4,7 +4,7 @@ namespace AdventOfCode2024.Solutions;
 
 public class Day18 : IDay
 {
-    public XyPair<int>[] Parse()
+    private XyPair<int>[] Parse()
     {
         return FileHelpers.ReadAndSplitLines<int>(18, ",")
             .Select(row => new XyPair<int>(row[0], row[1]))
@@ -44,31 +44,36 @@ public class Day18 : IDay
         return neighbours;
     }
 
-    private Dictionary<XyPair<int>, int> Dijkstra(HashSet<XyPair<int>> corruptions, int width, int height)
+    private Dictionary<XyPair<int>, (int, XyPair<int>?)> Dijkstra(HashSet<XyPair<int>> corruptions, int width, int height)
     {
         var start = new XyPair<int>(0, 0);
         var neighbours = Neighbours(corruptions, width, height);
 
         var active = new HashSet<XyPair<int>> { start };
 
-        var distances = new Dictionary<XyPair<int>, int>();
+        var distances = new Dictionary<XyPair<int>, (int, XyPair<int>?)>();
         foreach (var neighbour in neighbours.Keys)
-            distances.Add(neighbour, int.MaxValue);
-        distances[start] = 0;
+            distances.Add(neighbour, (int.MaxValue, null));
+        distances[start] = (0, null);
 
         while (active.Count > 0)
         {
-            var source = active.MinBy(coord => distances[coord]);
-            var sourceDistance = distances[source];
+            var source = active.MinBy(coord => distances[coord].Item1);
+            var (sourceDistance, _) = distances[source];
 
-            foreach (var neighbour in neighbours[source].Where(n => !active.Contains(n)))
+            foreach (var neighbour in neighbours[source])
             {
-                var distance = sourceDistance + 1;
-                if (distance < distances[neighbour])
-                {
-                    distances[neighbour] = distance;
-                    active.Add(neighbour);
-                }
+                if (active.Contains(neighbour))
+                    continue;
+
+                var (previousDistance, _) = distances[neighbour];
+                var newDistance = sourceDistance + 1;
+                
+                if (newDistance >= previousDistance)
+                    continue;
+                
+                distances[neighbour] = (newDistance, source);
+                active.Add(neighbour);
             }
 
             active.Remove(source);
@@ -77,22 +82,51 @@ public class Day18 : IDay
         return distances;
     }
 
+    private HashSet<XyPair<int>> GetVisited(Dictionary<XyPair<int>, (int, XyPair<int>?)> distances, XyPair<int> end)
+    {
+        var visited = new HashSet<XyPair<int>>();
+        var current = end;
+
+        while (true)
+        {
+            visited.Add(current);
+
+            var (_, previous) = distances[current];
+
+            if (previous is null)
+                break;
+
+            current = previous.Value;
+        }
+
+        return visited;
+    }
+
     public int Part1(XyPair<int>[] corruptions, int width, int height)
     {
         var distances = Dijkstra(corruptions.ToHashSet(), width, height);
         var end = new XyPair<int>(width - 1, height - 1);
-        return distances[end];
+        var (distance, _) = distances[end];
+        return distance;
     }
 
     public XyPair<int> Part2(XyPair<int>[] corruptions, int width, int height, int atLeast)
     {
         var end = new XyPair<int>(width - 1, height - 1);
+        var length = atLeast;
         
-        for (var length = atLeast; length < corruptions.Length; length++)
+        while (length < corruptions.Length)
         {
-            var distances = Dijkstra(corruptions.Take(length).ToHashSet(), width, height);
-            if (distances[end] == int.MaxValue)
+            var bytes = corruptions.Take(length).ToHashSet();
+            var distances = Dijkstra(bytes, width, height);
+            var (distance, _) = distances[end];
+            if (distance == int.MaxValue)
                 return corruptions[length - 1];
+
+            length++;
+            var visited = GetVisited(distances, end);
+            while (!visited.Contains(corruptions[length - 1]))
+                length++;
         }
 
         return new XyPair<int>(-1, -1);
