5b66998230f6f35e6665cf71f843d65cb8fb3cbb
jamsidedown <46314954+jamsidedown@users.noreply.github.com>
2024-12-24 18:40:33 +0000

    Day 22 runs in under a second at last

--- AdventOfCode2024.Test/Day22Tests.cs
+++ AdventOfCode2024.Test/Day22Tests.cs
@@ -4,8 +4,6 @@ namespace AdventOfCode2024.Test;
 
 public class Day22Tests
 {
-    private readonly long[] _exampleInput = [1, 2, 3, 2024];
-    
     private readonly Day22 _solution = new();
 
     [Fact]
@@ -15,12 +13,5 @@ public class Day22Tests
         Assert.Equal(4700978, _solution.SecretNumber(10, 2000));
         Assert.Equal(15273692, _solution.SecretNumber(100, 2000));
         Assert.Equal(8667524, _solution.SecretNumber(2024, 2000));
-    }
-
-    [Fact]
-    public void TestPart2()
-    {
-        var result = _solution.Sell(_exampleInput, (-2, 1, -1, 3));
-        Assert.Equal(23, result);
     }
 }
--- AdventOfCode2024/Solutions/Day22.cs
+++ AdventOfCode2024/Solutions/Day22.cs
@@ -31,9 +31,9 @@ public class Day22 : IDay
         return current;
     }
 
-    private Dictionary<long, Dictionary<SellCondition, Price>> _changes = new();
+    private Dictionary<SellCondition, Price> _prices = new();
     
-    private void Cache(long input)
+    private void CalculatePrices(long input)
     {
         var prices = new int[2000];
         var changes = new int[2000];
@@ -52,55 +52,33 @@ public class Day22 : IDay
 
             previousPrice = price;
         }
+        
+        var seen = new HashSet<SellCondition>();
 
-        var cache = new Dictionary<SellCondition, Price>();
         for (var i = 3; i < 2000; i++)
         {
             var sellCondition = (changes[i - 3], changes[i - 2], changes[i - 1], changes[i]);
-            if (!cache.ContainsKey(sellCondition))
-                cache[sellCondition] = prices[i];
-        }
+            if (!seen.Contains(sellCondition))
+            {
+                if (_prices.ContainsKey(sellCondition))
+                    _prices[sellCondition] += prices[i];
+                else
+                    _prices[sellCondition] = prices[i];
 
-        _changes[input] = cache;
+                seen.Add(sellCondition);
+            }
+        }
     }
     
-    private int Sell(long input, SellCondition sellCondition)
-    {
-        if (!_changes.ContainsKey(input))
-            Cache(input);
-
-        var cache = _changes[input];
-
-        return cache.GetValueOrDefault(sellCondition, 0);
-    }
-
-    public int Sell(long[] numbers, (int, int, int, int) sellCondition) =>
-        numbers.Sum(number => Sell(number, sellCondition));
-
     private long Part1(long[] numbers) =>
         numbers.Sum(n => SecretNumber(n, 2000));
 
     private int Part2(long[] numbers)
     {
-        var maximum = 0;
-        
-        for (var a = -9; a <= 9; a++)
-        for (var b = -9; b <= 9; b++)
-        {
-            Console.WriteLine($"{a}, {b}");
-            
-            for (var c = -9; c <= 9; c++)
-            for (var d = -9; d <= 9; d++)
-            {
-                var sell = (a, b, c, d);
-                var total = Sell(numbers, sell);
-                
-                if (total > maximum)
-                    maximum = total;
-            }
-        }
+        foreach (var number in numbers)
+            CalculatePrices(number);
 
-        return maximum;
+        return _prices.Values.Max();
     }
 
     public void Run()
